e6d7e9cd9bc01441cacdf6940ca96fc78d5fe380
Md. Rony <ronimdrahim467@gmail.com>
2025-08-20 16:05:51 +0600

    first commit

---
+++ js/child.js
@@ -0,0 +1,19 @@
+let new_child=document.createElement('li');
+new_child.innerText="This The Added text";
+
+let list=document.getElementById('countrys');
+
+list.appendChild(new_child);
+
+let anather_list=document.createElement('li');
+anather_list.innerText='ami to paitesi list add korte';
+
+let new_add=document.getElementById('countrys');
+
+new_add.appendChild(anather_list);
+
+let a_rekList=document.createElement('ul')
+a_rekList.innerText="Eita ar Akta ul List banaya fellam";
+
+let new_add2=document.getElementById('countrys');
+new_add2.appendChild(a_rekList);
---
+++ js/frist.js
@@ -0,0 +1,8 @@
+let sections=document.querySelectorAll("section");
+
+for(let single_section of sections){
+
+    console.log(single_section);
+    // single_section.classList.add('bg-red')
+    single_section.style.backgroundColor='green'
+}
